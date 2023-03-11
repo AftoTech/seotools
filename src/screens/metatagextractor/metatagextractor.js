b2c26a1dca23e17b1611bd9ef48d60cd01ac5000
AftoTech <afto.tech2021@gmail.com>
2023-03-11 12:35:52 +0530

    metaextractor

--- src/screens/metatagextractor/metatagextractor.js
+++ src/screens/metatagextractor/metatagextractor.js
@@ -1,22 +1,37 @@
-
-import React from 'react';
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-import './metatagextcator.css';
+import { BASE_URL } from "../../constant";
+
+import "./metatagextcator.css";
 const MetaTagExtractor = () => {
-    
-    const Extract = async() => {
-        var url = document.getElementById("inputField").value;
-        console.log(url)
-    //    const data = await getMetaData(url)
-	// console.log(data)
-        
-    }
-    return (
-        <div className="container">
+  
+  const [metaextracted, setmetaextracted] = useState();
+  const [Loading ,setLoading] = useState(false)
+
+  const Extract = async () => {
+    setLoading(true)
+    var url = document.getElementById("inputField").value;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ url: url }),
+    };
+    fetch(BASE_URL + '/metatagextract', requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+         setmetaextracted(data.meta);
+        setLoading(false)
+      }).catch((err) => {
+        console.log(err)
+          setLoading(false)
+      });
+  };
+  return (
+    <div className="container">
       <div>
         <p>MetaTagExtractor :</p>
-              <textarea
-                  className="Metatagarea"
+        <textarea
+          className="Metatagarea"
           id="inputField"
           placeholder="Enter your Url Here"
         ></textarea>
@@ -24,13 +39,17 @@ const MetaTagExtractor = () => {
       <div className="extractbutton">
         <button onClick={Extract}>Generate</button>
       </div>
-     <div>
+      <div>
         <p>Result :</p>
-        <p></p>
+        <p>{
+          Loading ? 
+         <p>Loading....</p>
+            :
+            <p>{JSON.stringify(metaextracted)}</p>
+        }</p>
       </div>
-    
     </div>
-  )
-}
- 
+  );
+};
+
 export default MetaTagExtractor;
