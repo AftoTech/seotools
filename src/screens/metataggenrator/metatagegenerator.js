23d338b62a2839dbcd47b4a517a97a30cd633602
AftoTech <afto.tech2021@gmail.com>
2023-04-10 08:11:27 +0530

    metataggenerator

--- src/screens/metataggenrator/metatagegenerator.js
+++ src/screens/metataggenrator/metatagegenerator.js
@@ -3,6 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 import "./metataggenerator.css";
 const MetaTagGenerator = () => {
   const [Result, setResult] = useState("");
+  const [Copied ,setCopied] = useState('')
   function create() {
     let form = "";
     var Titletext = document.getElementById("Title").value;
@@ -28,87 +29,86 @@ const MetaTagGenerator = () => {
 
   return (
     <div>
-      <h1>MetaTagGenerator</h1>
-      <div align="center">
-        <center>
-          <table
-            class="normal"
-            WIDTH="600"
-            BORDER="0"
-            CELLSPACING="0"
-            CELLPADDING="0"
-          >
-            <tr>
-              <td>
-                <center>
-                  <font face="verdana" size="4">
-                    Meta tag generator
-                  </font>
-                </center>
-
-                <form>
-                  <div align="center">
-                    <center>
-                      <table class="normal" BORDER="0" CELLPADDING="2">
-                        <tr>
-                          <td>Title:</td>
-                          <td>
-                            <input
-                              id="Title"
-                              TYPE="text"
-                              NAME="input1"
-                              SIZE="40"
-                            />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Author:</td>
-                          <td>
-                            <input
-                              id="Author"
-                              TYPE="text"
-                              NAME="input2"
-                              SIZE="40"
-                            />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Discription:</td>
-                          <td>
-                            <input
-                              id="Discription"
-                              TYPE="text"
-                              NAME="input3"
-                              SIZE="40"
-                            />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Keyword:</td>
-                          <td>
-                            <input
-                              id="Keyword"
-                              TYPE="text"
-                              NAME="input4"
-                              SIZE="40"
-                            />
-                          </td>
-                        </tr>
-                      </table>
-                    </center>
-                  </div>
-                </form>
-                <div className="calculatebutton">
-                  <button onClick={create}>Generate</button>
-                </div>
-              </td>
-            </tr>
-          </table>
-        </center>
-        <div>
-          <p>Result :</p>
+      {/* Header */}
+      <div class="header">
+        <div class="headerlogo">
+          <p class="HeaderTitle">MetaTagGenerator</p>
+        </div>
+      </div>
+      <div className="inputSectionContainer">
+        <div className="inputtitlecontainer">
+          <p>Input :</p>
+          <form>
+            <div>
+              <center>
+                <table class="normal" BORDER="0" CELLPADDING="1">
+                  <tr>
+                    <td>Title:</td>
+                    <td>
+                      <input id="Title" TYPE="text" NAME="input1" SIZE="40" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Author:</td>
+                    <td>
+                      <input id="Author" TYPE="text" NAME="input2" SIZE="40" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Discription:</td>
+                    <td>
+                      <input
+                        id="Discription"
+                        TYPE="text"
+                        NAME="input3"
+                        SIZE="40"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Keyword:</td>
+                    <td>
+                      <input id="Keyword" TYPE="text" NAME="input4" SIZE="40" />
+                    </td>
+                  </tr>
+                </table>
+              </center>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="calculatebutton">
+        <button className="buttonstyle" onClick={create}>
+          Extract
+        </button>
+      </div>
+      <div className="ResultSection">
+        <p>Result :</p>
+        <div
+          className="teatAreastyle"
+          style={{ height: 100, marginBottom: 100 }}
+        >
+          <div>
+            {
+              Result && (
+                 <button
+              onClick={async () => {
+                try {
+                  await navigator.clipboard.writeText(Result);
+                  alert('copied')
+                } catch (err) {
+  setCopied('')
+                }
+              }}
+            >
+              Copy
+            </button>
+              )
+           }
+          </div>
+          
           <div>{Result}</div>
-        </div>  
+        </div>
       </div>
     </div>
   );
